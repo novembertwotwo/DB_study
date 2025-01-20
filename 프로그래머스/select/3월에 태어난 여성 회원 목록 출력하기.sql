1f29fce6d42ec2220283c1fb5787ede54752fc8b
novembertwotwo <korea621214@naver.com>
2025-01-20 16:29:59 +0900

    3월에 태어난 여성 회원 목록 출력하기

---
+++ 프로그래머스/select/3월에 태어난 여성 회원 목록 출력하기.sql
@@ -0,0 +1,9 @@
+select MEMBER_ID,MEMBER_NAME,GENDER,DATE_FORMAT(DATE_OF_BIRTH,"%Y-%m-%d") 
+from MEMBER_PROFILE 
+where GENDER = 'W' && TLNO is not null && MONTH(DATE_OF_BIRTH) = '3';
+
+
+-- 배운것
+DATE_FORMAT(DATE_OF_BIRTH,"%Y-%m-%d")
+MONTH(DATE_OF_BIRTH) = '3'
+TLNO is not null
